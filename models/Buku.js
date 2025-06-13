519e57704ff948478a833d60e6b713adb0cee591
Mochamad Noer Faizi Supriatna Putra <noerfaizim@gmail.com>
2025-06-13 23:10:08 +0700

    update

--- models/Buku.js
+++ models/Buku.js
@@ -1,41 +1,56 @@
-const { DataTypes } = require('sequelize');
-const { sequelize } = require('../config/database'); // <-- Perubahan di sini!
-const User = require('./User');
+'use strict';
+const { Model } = require('sequelize');
 
-const Buku = sequelize.define('Buku', {
-  id: {
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
-    primaryKey: true,
-    allowNull: false,
-  },
-  nama_buku: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  penulis_buku: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  gambar: {
-    type: DataTypes.STRING, // Path atau URL gambar
-    allowNull: true,
-  },
-  userId: { // Ini akan menjadi foreign key ke tabel User
-    type: DataTypes.UUID,
-    references: {
-      model: User,
-      key: 'id',
+module.exports = (sequelize, DataTypes) => {
+  class Buku extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // Define association here
+      // A Buku (book) belongs to one User
+      this.belongsTo(models.User, {
+        foreignKey: 'userId',
+        as: 'user',
+      });
+    }
+  }
+
+  Buku.init({
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
     },
-    allowNull: false,
-  },
-}, {
-  timestamps: true, // createdat dan updatedat otomatis
-  tableName: 'bukus', // Nama tabel di database
-});
+    nama_buku: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    penulis_buku: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    gambar: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    userId: { // This is the foreign key
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: 'Users', // Note: It's common practice to reference the table name here
+        key: 'id',
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Buku',
+    tableName: 'bukus', // Ensure this matches your database table name
+    timestamps: true,
+  });
 
-// Definisikan relasi
-User.hasMany(Buku, { foreignKey: 'userId', as: 'bukus' });
-Buku.belongsTo(User, { foreignKey: 'userId', as: 'user' });
-
-module.exports = Buku;
+  return Buku;
+};
--- models/User.js
+++ models/User.js
@@ -3,41 +3,44 @@ const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
     static associate(models) {
-      // definisikan asosiasi di sini jika ada
-      User.hasMany(models.buku, { foreignKey: 'userId' }); // Contoh jika ada relasi ke buku
+      // Define association here
+      // A User can have many Bukus (books)
+      this.hasMany(models.Buku, {
+        foreignKey: 'userId',
+        as: 'bukus',
+      });
     }
   }
+
   User.init({
     id: {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
       primaryKey: true,
-      allowNull: false
+      allowNull: false,
     },
-    googleId: {
+    username: {
       type: DataTypes.STRING,
+      allowNull: false,
       unique: true,
-      allowNull: true
     },
-    name: { // Menggunakan 'name' bukan 'nama'
+    password: {
       type: DataTypes.STRING,
-      allowNull: false
-    },
-    email: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false
-    },
-    profilePic: {
-      type: DataTypes.STRING,
-      allowNull: true
+      allowNull: false,
     }
-    // Tidak ada kolom password di sini
+    // ... other user fields
   }, {
     sequelize,
     modelName: 'User',
-    tableName: 'Users' // Eksplisit menyatakan nama tabel
+    tableName: 'Users', // Ensure this matches your database table name
+    timestamps: true,
   });
+
   return User;
 };
