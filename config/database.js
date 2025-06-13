6aad813271fc3b7b5a4467c0d2efcd81c549ce14
Mochamad Noer Faizi Supriatna Putra <noerfaizim@gmail.com>
2025-06-13 21:53:22 +0700

    update

---
+++ config/database.js
@@ -0,0 +1,24 @@
+const { Sequelize } = require('sequelize');
+const env = process.env.NODE_ENV || 'development';
+const config = require('./config')[env];
+
+let sequelize;
+
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], {
+    dialect: config.dialect,
+    dialectOptions: config.dialectOptions || {},
+    logging: false, // Matikan logging SQL di production
+  });
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: config.dialect,
+    logging: false,
+  });
+}
+
+module.exports = {
+  sequelize,
+  Sequelize,
+};
--- models/Buku.js
+++ models/Buku.js
@@ -1,38 +1,37 @@
-// models/Buku.js
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('../config/database');
-const User = require('./User'); // Import model User
+const { sequelize } = require('../config/database'); // <-- Perubahan di sini!
+const User = require('./User');
 
 const Buku = sequelize.define('Buku', {
-    id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
-        allowNull: false
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+    allowNull: false,
+  },
+  nama_buku: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  penulis_buku: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  gambar: {
+    type: DataTypes.STRING, // Path atau URL gambar
+    allowNull: true,
+  },
+  userId: { // Ini akan menjadi foreign key ke tabel User
+    type: DataTypes.UUID,
+    references: {
+      model: User,
+      key: 'id',
     },
-    nama_buku: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    penulis_buku: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    gambar: {
-        type: DataTypes.STRING, // Path atau URL gambar
-        allowNull: true
-    },
-    userId: { // Ini akan menjadi foreign key ke tabel User
-        type: DataTypes.UUID,
-        references: {
-            model: User,
-            key: 'id'
-        },
-        allowNull: false
-    }
+    allowNull: false,
+  },
 }, {
-    timestamps: true, // createdat dan updatedat otomatis
-    tableName: 'bukus' // Nama tabel di database
+  timestamps: true, // createdat dan updatedat otomatis
+  tableName: 'bukus', // Nama tabel di database
 });
 
 // Definisikan relasi
